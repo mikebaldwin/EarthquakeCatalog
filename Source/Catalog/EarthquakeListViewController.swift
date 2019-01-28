b2e3778331d21c86870421990e131a1f6c04d666
Michael Baldwin <baldwin@BaldwinMacbookPro.local>
2019-01-28 12:17:24 -0800

    Set up app structure

---
+++ Source/Catalog/EarthquakeListViewController.swift
@@ -0,0 +1,38 @@
+//
+//  EarthquakeListViewController.swift
+//  EarthquakeCatalog
+//
+//  Created by Michael Baldwin on 1/28/19.
+//  Copyright © 2019 mikebaldwin.co. All rights reserved.
+//
+
+import UIKit
+
+class EarthquakeListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
+// MARK: - Table view data source
+extension EarthquakeListViewController {
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+}
---
+++ Source/Networking/EarthquakeCatalogApi.swift
@@ -0,0 +1,13 @@
+//
+//  EarthquakeCatalogApi.swift
+//  EarthquakeCatalog
+//
+//  Created by Michael Baldwin on 1/28/19.
+//  Copyright © 2019 mikebaldwin.co. All rights reserved.
+//
+
+import Foundation
+
+class EarthquakeCatalogApi {
+    
+}
